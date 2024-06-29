069e8ba8d264b0a0a9d0709791f064f749bf7f4c
Abu Taher Muhammad <abut1081@gmail.com>
2024-06-30 02:41:39 +0600

    fix: solved on-page tw class issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./components/**/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -18,7 +18,7 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
-  important: "#__next",
+  // important: "#__next",
   plugins: [],
 }
 
